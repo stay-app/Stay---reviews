035e9a31c306e38feb22867ef6358a52a2f13a83
Jinjing <bijinjing88@gmail.com>
2019-08-29 16:32:12 -0700

    update the css for the reveiw commnets

--- client/components/Review.jsx
+++ client/components/Review.jsx
@@ -3,25 +3,47 @@ import ReadMore from './ReadMore.jsx';
 import moment from 'moment';
 import styled from 'styled-components'
 
-const profileImg = styled.img`
-  height='1'
-  width='1'
-`
+const Review_profile = styled.div`
+  font-size: 14px;
+  font-family: Circular, -apple-system, BlinkMacSystemFont, Roboto, Helvetica Neue, sans-serif;
+  line-height: 1.43;
+  color: #484848;
+  background-color: #fff;
 
-const profileName = styled.div`
-  width:788px;
-  color:green
-`
 
+`;
+
+const Profile = styled.div`
+  display:inline-flex;
+  align-items: flex-start;
+  margin-top:20px;
+  margin-bottom:17px
+`;
+const ProfileTitle = styled.div`
+  display:inline-block;
+  margin-left:20px;
+
+`;
+
+const ProfileImg = styled.img`
+align-items: flex-start;
+display:inline-block;
+  height:53px;
+  border-Radius:50%
+`
 
 const Review = (props) => {
   return(
-    <div>
-      <img style={{height:`50px`, borderRadius: `50%`}} src={props.data.profile}></img>
-      <div style={{}}>{props.data.name}</div>
-      <div>{moment(props.data.review_date).format('MMMM YYYY')}</div>
+    <Review_profile>
+      <Profile>
+        <ProfileImg src={props.data.profile}></ProfileImg>
+        <ProfileTitle>
+          <div style={{fontWeight:`bold`,marginBottom:'3px'}}>{props.data.name}</div>
+          <div>{moment(props.data.review_date).format('MMMM YYYY')}</div>
+        </ProfileTitle>
+      </Profile>
       {!props.searchStatus && props.data.comments.split(" ").length > 30 ? <ReadMore comments = {props.data.comments}/> : <div>{props.data.comments}</div>}
-    </div>
+    </Review_profile>
 
   )
 }
--- db/dataGenerate/user.js
+++ db/dataGenerate/user.js
@@ -13,10 +13,12 @@ con.connect();
 
 for( let i=0; i <1000; i++) {
   let randomName = faker.name.findName();
+  let randomCity = faker.address.city();
+  let randomState = faker.address.stateAbbr();
   let randomrawprofile = faker.image.avatar();
 
   const inputMessage = "INSERT INTO customer set ?";
-  const option = {name:randomName, profile:randomrawprofile}
+  const option = {name:randomName, city:randomCity, state:randomState, profile:randomrawprofile}
 
   con.query(inputMessage, option, (error, insertID) => {
     if (error) throw error;
